98ae4466acfbf68f9bf064948652350b5bac220e
chrilleson <Christoffer.Ljungqvist@hotmail.se>
2017-10-30 15:19:32 +0100

    Added informative comments

--- Labb5/Labb5/MainWindow.xaml.cs
+++ Labb5/Labb5/MainWindow.xaml.cs
@@ -25,9 +25,10 @@ namespace Labb5
             InitializeComponent();
         }
 
+        //Knapp för att skapa en ny användare
         private void ButtonSkapa_Click(object sender, RoutedEventArgs e)
         {
-            if (TextBoxNamn.Text != "" && TextBoxEpost.Text != "")
+            if (TextBoxNamn.Text != "" && TextBoxEpost.Text != "")  //If-sats för att kolla så att textboxarna inte är tomma
             {
                 normalUsersListbox.Items.Add(new User(TextBoxNamn.Text, TextBoxEpost.Text));
                 TextBoxNamn.Clear();
@@ -35,13 +36,15 @@ namespace Labb5
             }
         }
 
+        //Knapp för att ta bort en cald användare
         private void buttonTaBort_Click_1(object sender, RoutedEventArgs e)
         {
+            //If-sats som kollar om amn har valt en användare i listan med vanliga användare eller i Admin listan, tar sedan bort den valda användaren
             if (normalUsersListbox.SelectedIndex >= 0)
                 normalUsersListbox.Items.Remove(normalUsersListbox.Items[normalUsersListbox.SelectedIndex]);
             else if (AdminListBox.SelectedIndex >= 0)
                 AdminListBox.Items.Remove(AdminListBox.Items[AdminListBox.SelectedIndex]);
-
+            //Sätter knapparna som ska vara disabled till disabled
             ChangeUser.IsEnabled = false;
             ButtonChangeToAdmin.IsEnabled = false;
             buttonTaBort.IsEnabled = false;
@@ -50,6 +53,7 @@ namespace Labb5
 
         private void normalUsersListbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //If-sats som kollar om man valt en användare i den normala listan och gör sedan rätt knappar enabled
             if (normalUsersListbox.SelectedIndex >= 0)
             {
                 ChangeUser.IsEnabled = true;
@@ -59,15 +63,20 @@ namespace Labb5
             }
         }
 
+        //Knapp för att ändra en vald admin till normal användare
         private void ButtonChangeToUser_Click(object sender, RoutedEventArgs e)
         {
+            //Lägger till den valda användaren till admin listan och tar bort från den gamla listan
             normalUsersListbox.Items.Add(AdminListBox.SelectedItem);
             AdminListBox.Items.Remove(AdminListBox.SelectedItem);
+            
+            //Sätter knapparna som ska vara disabled till disabled
             ChangeUser.IsEnabled = false;
             ButtonChangeToAdmin.IsEnabled = false;
             buttonTaBort.IsEnabled = false;
             ButtonChangeToUser.IsEnabled = false;
         }
+        //Knapp för att få information om den valda änvändaren
         private void buttonInfo_Click(object sender, RoutedEventArgs e)
         {
             if (normalUsersListbox.SelectedIndex >= 0)
@@ -85,10 +94,14 @@ namespace Labb5
             }
         }
 
+        //Knapp för att ändra en normal användare till admin
         private void ButtonChangeToAdmin_Click_1(object sender, RoutedEventArgs e)
         {
+            //Lägger till den valda adminen till användar listan och tar bort från den gamla listan
             AdminListBox.Items.Add(normalUsersListbox.SelectedItem);
             normalUsersListbox.Items.Remove(normalUsersListbox.SelectedItem);
+
+            //Sätter knapparna som ska vara disabled till disabled
             ChangeUser.IsEnabled = false;
             ButtonChangeToAdmin.IsEnabled = false;
             buttonTaBort.IsEnabled = false;
@@ -97,6 +110,7 @@ namespace Labb5
 
         private void AdminListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //If-sats som kollar om man valt en användare i admin listan och gör sedan rätt knappar enabled
             if (AdminListBox.SelectedIndex >= 0)
             {
                 ChangeUser.IsEnabled = true;
@@ -106,6 +120,7 @@ namespace Labb5
             }
         }
 
+        //Knapp för att ändra en vald användare eller admin
         private void ChangeUser_Click(object sender, RoutedEventArgs e)
         {
             if (normalUsersListbox.SelectedIndex >= 0)
